303cfcc65f168019c6e66011afef6f69f6fce23b
Andy McBlane <andymcblane@hotmail.com>
2022-07-17 12:30:12 +1000

    Tidy imports

--- code/main.py
+++ code/main.py
@@ -1,13 +1,6 @@
-import re
 from selenium import webdriver
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.keys import Keys
 import time 
-import json 
-from selenium.common.exceptions import ElementClickInterceptedException
-import subprocess
-from pathlib import Path
 import os 
 
 try:
